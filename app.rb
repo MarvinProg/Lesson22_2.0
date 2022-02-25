3c5ac9c0fd7b98adb4901a86153e3900026f2c88
lomakin <lomakin-as@mail.ru>
2022-02-25 15:30:01 +0300

    upgrade: Add link on pages views and add method get for page

--- app.rb
+++ app.rb
@@ -6,3 +6,15 @@ require 'sinatra/reloader'
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
+
+get '/about_us' do 
+	erb :about_us
+end
+
+get '/appointment' do 
+	erb :appointment
+end
+
+get '/contacts' do 
+	erb :contacts
+end
